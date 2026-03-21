573a838a0851c46afe89166b7421348a0c0e5291
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-21 23:27:24 +0530

    TODO apk

--- TODOAPK/app.js
+++ TODOAPK/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+
+// const Todoroute = require("./routes/todoroute")
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/",(req,res)=>{
+    res.render("home")
+})
 
 app.listen(3000,(()=>{
     console.log("Server is running on port http://localhost:3000");
