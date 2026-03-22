305c3460f7154eb8239590a0b5cdf99b44589451
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-23 03:50:48 +0530

    complete todocontroller

--- TODOAPK/controllers/todocontrollers.js
+++ TODOAPK/controllers/todocontrollers.js
@@ -1,15 +1,52 @@
 const Todo = require('../models/Todo');
 
+exports.getTodo =async (req, res) => {
+    try{
+        const todos = await Todo.find().lean(); //find data
+        res.render("home",{todos}) // pass ejs
+    }catch(error){
+        console.error("error")
+        res.status(500).send("Error fetching todos")
+    } 
+};
 
-exports.getTodo = (req,res)=>{
-    console.log("jii haa kya baat hai 2")
+exports.createTodo = async (req, res) => {
+    try {
+        console.log(req.body);
 
-    res.render("home")
+        const { Work } = req.body; 
+
+        const newTODO = await Todo.create({
+            title: Work,
+        });
+
+        console.log("Saved:", newTODO);
+
+        res.redirect("/"); 
+    } catch (error) {
+        console.error("Error saving todo", error);
+        res.status(500).send("Server Error");
+    }
+};
+
+exports.deleteTodo = async (req,res)=>{
+    try{
+        await Todo.findByIdAndDelete(req.params.id) // find and delete
+        res.redirect("/")
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Delete error")
+    }
 }
 
-exports.createTodo = (req,res)=>{
-    const {Work} = req.body;
-    console.log(req.body)
-    Todo.create({Work})
-    res.render("home")
+exports.toggleTodo = async(req,res) =>{
+    try {
+        const todo =  await Todo.findById(req.params.id)  //find data using dynamic id
+        todo.completed = !todo.completed;
+        await todo.save();
+        res.redirect("/")
+    }catch(error){
+        console.error(error)
+        res.status(500).send("Toggle error")
+    }
 }
