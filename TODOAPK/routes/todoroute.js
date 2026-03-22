e326d7346d5bfec29a6a1db53a7fc6500337adf8
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-22 23:24:29 +0530

    Update

--- TODOAPK/routes/todoroute.js
+++ TODOAPK/routes/todoroute.js
@@ -2,4 +2,9 @@ const express = require("express")
 const route = express.Router();
 const todocontroller = require("../controllers/todocontrollers")
 
+console.log("jii haa kya baat hai no:1 ")
+
 route.get("/",todocontroller.getTodo);
+route.post("/submit",todocontroller.createTodo);
+
+module.exports = route;
