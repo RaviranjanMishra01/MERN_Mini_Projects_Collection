44897eac314fd719ec2ead66bd067466807ff35f
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-22 23:25:05 +0530

    update

--- TODOAPK/app.js
+++ TODOAPK/app.js
@@ -1,16 +1,36 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+// const express = require("express");
+// const app = express();
+// const path = require("path");
 
 
 // const Todoroute = require("./routes/todoroute")
 
+
+// app.set("view engine", "ejs");
+// app.use(express.static("public"));
+// app.use(express.urlencoded({extended:true}))
+
+// console.log("jii haa kya baat hai")
+// app.use("/",Todoroute)
+
+// app.listen(3000,(()=>{
+//     console.log("Server is running on port http://localhost:3000");
+// }));
+
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const Todoroute = require("./routes/todoroute");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.get("/",(req,res)=>{
-    res.render("home")
-})
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000,(()=>{
-    console.log("Server is running on port http://localhost:3000");
-}));
+// ✅ Correct way to use router
+app.use("/", Todoroute);
+
+app.listen(3000, () => {
+  console.log("Server is running on port http://localhost:3000");
+});
