dd4bc15ea6d2be30243dbaf13ef6fd57391b2ad2
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-23 03:52:14 +0530

    Create a new router

--- TODOAPK/routes/todoroute.js
+++ TODOAPK/routes/todoroute.js
@@ -6,5 +6,7 @@ console.log("jii haa kya baat hai no:1 ")
 
 route.get("/",todocontroller.getTodo);
 route.post("/submit",todocontroller.createTodo);
+route.post("/delete/:id",todocontroller.deleteTodo);
+route.post("/toggle/:id",todocontroller.toggleTodo);
 
 module.exports = route;
