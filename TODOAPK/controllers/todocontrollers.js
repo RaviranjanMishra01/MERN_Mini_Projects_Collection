a6cd45a53d04d81903c2b810acd7f428faa2cacb
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-22 23:22:49 +0530

    Update

--- TODOAPK/controllers/todocontrollers.js
+++ TODOAPK/controllers/todocontrollers.js
@@ -1,7 +1,15 @@
-// const Todo = require('../models/Todo');
+const Todo = require('../models/Todo');
+
+
+exports.getTodo = (req,res)=>{
+    console.log("jii haa kya baat hai 2")
+
+    res.render("home")
+}
 
 exports.createTodo = (req,res)=>{
-    // const {Work} = req.body;
-    // await Todo.create({Work})
-    res.redirect("/")
+    const {Work} = req.body;
+    console.log(req.body)
+    Todo.create({Work})
+    res.render("home")
 }
