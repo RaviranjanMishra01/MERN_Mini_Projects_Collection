3761df9e564ec07b891b1b301c15b92418b58c64
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-24 23:03:05 +0530

    update

--- notes-app-mern_02/index.js
+++ notes-app-mern_02/index.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose")
+const Route = require("./routes/noteroute")
+const DB_URL = "mongodb+srv://Ravi01:Ravi01@cluster0.g6uewf4.mongodb.net/?appName=Cluster0"
 
-app.get("/",(req,res)=>{
-    console.log("hyy bou")
-    res.send("<h3>hyy jii wait </h3>")
-})
+app.set("view engine","ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
 
-app.listen(3000,(()=>{
-    console.log("Server is running on port http://localhost:3000");
-}));
+
+app.use("/", Route)
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        const PORT = 3000
+        console.log("Mongodb connected")
+        app.listen(PORT, () => {
+            console.log(`Server is Starts on http://localhost:${PORT}`)
+        })
+    })
+    .catch(err => {
+        console.log("Error mongodb connection failed", err)
+    })
