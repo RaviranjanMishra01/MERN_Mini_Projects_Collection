edb7dd06b499d51f3d1432692c1739fa457cc1ee
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-25 14:08:31 +0530

    fully working

--- notes-app-mern_02/routes/noteroute.js
+++ notes-app-mern_02/routes/noteroute.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const route = express.Router();
+const note_control = require("../Controller/notes_controller.js")
 
 
 console.log("router runing")
-route.get("/",(req,res)=>{res.render("index")})
+route.get("/",note_control.getNotes)
+route.post("/addnote",note_control.CreateNote)
+route.post("/note/:id/toggle", note_control.toggleMark);
 
 
 module.exports = route
